b54f917d91f995bcf3aace130d8c752785ad04fb
Ekene Ezema <ekenestanekeson@gmail.com>
2022-08-23 23:20:19 +0100

    Added file

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - transverses a binary tree
+ * @tree: tree to be preordered
+ * @func: function to be called on each node
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	func(tree->n);
+	binary_tree_preorder(tree->left);
+	binary_tree_preorder(tree->right);
+}
+

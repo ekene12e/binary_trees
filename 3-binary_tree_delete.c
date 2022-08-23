5dacb29a5ebc8dd8992edc8a0858e88dd9f7b6dd
Ekene Ezema <ekenestanekeson@gmail.com>
2022-08-23 22:40:41 +0100

    Added file

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_right - inserts a node to the left
+ * binary_tree_delete - inserts a node to the left
  * @tree: the parent node
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree)
-		{
-			binary_tree_delete(tree->right);
-			binary_tree_delete(tree->left);
-			free(tree);
-		}
+	{
+		binary_tree_delete(tree->right);
+		binary_tree_delete(tree->left);
+		free(tree);
+	}
 }
 

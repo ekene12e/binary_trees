eaa2245cef9944574041863c271b5708451cf6c1
Ekene Ezema <ekenestanekeson@gmail.com>
2022-08-23 21:53:03 +0100

    Added file

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,9 +18,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
-	if (parent->n > value)
-		parent->left = new_node;
-
+	parent->left = new_node;
 	return (new_node);
 }
 

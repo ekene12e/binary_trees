f32cb2e1376c36798e9dc1685d2304695dece1a9
Ekene Ezema <ekenestanekeson@gmail.com>
2022-08-24 19:31:55 +0100

    Added file

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,10 +6,10 @@
  *
  * Return: If tree is NULL, your function must return 0, else return height.
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
-	binary_tree_t *temp = tree;
+	const binary_tree_t *temp = tree;
 
 	if (tree == NULL)
 		return (depth);
